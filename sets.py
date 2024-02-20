57a4f5015c11cdcaec822657bb377834d3f0100b
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-02-20 07:16:12 +0700

    Update sets.py

--- sets.py
+++ sets.py
@@ -3,3 +3,17 @@
 intersection = album_set1 & album_set2
 intersection
 
+# Use intersection method to find the intersection of album_list1 and album_list2
+album_set1.intersection(album_set2)   
+
+# Find the difference in set1 but not set2
+album_set1.difference(album_set2)  
+
+# Find the union of two sets
+album_set1.union(album_set2)
+
+# Check if subset -> tap con
+set({"Back in Black", "AC/DC"}).issubset(album_set1) 
+
+
+
