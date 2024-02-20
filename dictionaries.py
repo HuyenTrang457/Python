ca68f4b2617c53bc5ace70bcfb5320518e2e9f93
HuyenTrang457 <155938605+HuyenTrang457@users.noreply.github.com>
2024-02-20 07:40:07 +0700

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+# Get all the keys in dictionary
+dict.keys() 
+
+# Get all the values in dictionary
+dict.values() 
+
+# Append value with key into dictionary
+dict['Graduation'] = '2007'
+
+# Delete entries by key
+del(dict['Thriller'])
+del(dict['Graduation'])
+
+# Verify the key is in the dictionary
+'The Bodyguard' in release_year_dict
+
