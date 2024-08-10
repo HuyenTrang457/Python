30c467e687ad57c88ba796a58db046d164e686fe
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-10 08:42:17 +0700

    Update input.py

--- input.py
+++ input.py
@@ -2,3 +2,7 @@ s= input('nhap 3 so: ')
 a= s.split()
 x, y, z= map(int,a)  #sử dụng map để ép kiểu từ str sang int 
 print(x+y+z)
+
+## Hoặc Cách 2
+x,y,z= map(int, input('nhap').split())
+print(x+y+z)
