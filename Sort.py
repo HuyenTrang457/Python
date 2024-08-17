073b543e9de3bed638a7b4886ef9f974a60399ed
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-17 16:20:48 +0700

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -17,3 +17,27 @@ def cmp(a,b):
 a=[1,5,3,2,7,6]
 a.sort(key= cmp_to_key(cmp))
 print(a)
+
+--------------MERGE SORT ----------------------
+
+a= list(map(int, input().split()))
+b=list(map(int, input().split()))
+n,m= len(a),len(b)
+i,j= 0,0
+while i<n and j<m:
+   if a[i]<b[j]:
+      print('a',i+1,sep="", end="  ")
+      i+=1
+   elif a[i]>b[j]:
+      print('b',j+1,sep="", end="  ")
+      j+=1
+   else:
+      print('b',j+1,sep="", end="  ")
+      j+=1
+
+while i<n:
+   print('a',i+1,sep="", end="  ")
+   i+=1 
+while j<m:
+   print('b',j+1,sep="", end="  ")
+   j+=1
