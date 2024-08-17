59742f45ccba4ad745383136f0856cf4f0e0e835
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-17 14:08:15 +0700

    Create Sort.py

---
+++ Sort.py
@@ -0,0 +1,18 @@
+from math import *
+a=[-2,-4,1,6,3]
+a.sort(key= lambda x:abs(x))
+print(a)
+
+------------------------cmp_to_key--------------
+from math import *
+from functools import cmp_to_key
+def cmp(a,b):
+   if a<b:
+      return -1
+   elif a>b:
+      return 1
+   else:
+      return 0
+a=[1,5,3,2,7,6]
+a.sort(key= cmp_to_key(cmp))
+print(a)
