dd2262a3303011a738acd045c5be3740c6cb4f4f
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-18 19:42:18 +0700

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -9,13 +9,21 @@ value = 1
 b= dict.fromkeys(a,value)
 print(b)
 -------------------------------------
-
+# get a values with key
+dict(a.get('fbh'))
 # Get all the keys in dictionary
 dict.keys() 
 
 # Get all the values in dictionary
 dict.values() 
-
+----------------- duyet keys+ value
+for x in a:
+  print(x,a[x],sep='->')
+cachs 2:
+ a={'school':'dbb','city':'hcm'}
+for (key,value) in a.items():
+   print(key +'->' +str(value))
+-----------------------------
 # Append value with key into dictionary
 dict['Graduation'] = '2007'
 
