ce778addfd383585653647176ebcccd8893971f6
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-18 21:05:18 +0700

    Update dict_Excercise.py

--- dict_Excercise.py
+++ dict_Excercise.py
@@ -1,10 +1,11 @@
 ## đếm số lượng khác nhau
 a=[1,2,3,2,2,4,4,5]
-b= dict({})
+b= dict({})  ## b= {}  --> type: dict
 for x in a:
    if x in b:
       b[x]+=1 
    else:
       b[x]=1 
+c= list(b.items()) # sap xep
 for x in b:
    print(x,b[x],sep=': ')
