f9f39b0ee2747d276b9b9305db517ed990e020a0
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-22 13:13:18 +0700

    Update dict_Counter_Excercise.py

--- dict_Counter_Excercise.py
+++ dict_Counter_Excercise.py
@@ -14,3 +14,26 @@ for x in a:
 c= list(b.items()) # sap xep
 for x in b:
    print(x,b[x],sep=': ')
+--------------------- Thêm xóa theo truy vẫn 
+a=[1,2,3,2,2,4,4,5]
+b= dict({})
+for x in a:
+   if x  in b:
+      b[x]+=1 
+   else:
+      b[x]=1 
+print(b)
+q= int(input('nhap q:'))
+for i in range(q):
+   tt,x= map(int, input().split())
+   if tt==1:
+      if x in b:
+         b[x]+=1 
+      else:
+         b[x]=1
+   elif tt==2:
+      if x in b:
+         b[x]-=1 
+         if b[x]==0:
+            b.pop(x)
+print(b)
