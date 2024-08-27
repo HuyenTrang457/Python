c68f406af59f7f663fe580e5f646dc21dc92cd40
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-27 08:34:13 +0700

    Update class_exercise.py

--- class_exercise.py
+++ class_exercise.py
@@ -1,1 +1,12 @@
-
+-------- LOP SINH VIEN---------------------
+from math import *
+class sinhVien:
+   def __init__(self,hoTen,diem):
+      self.hoTen= hoTen
+      self.diem= diem
+   def __str__(self):
+      return f'ho ten: {self.hoTen}\ndiem: {self.diem}'
+ten="nguyen thi huyen trang"
+diem=11
+a= sinhVien(ten,diem)
+print(a)
