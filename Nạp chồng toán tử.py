5215d644ff8fc9de64a9f19e0982deaeda037267
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-30 21:00:02 +0700

    Update Nạp chồng toán tử.py

--- Nạp chồng toán tử.py
+++ Nạp chồng toán tử.py
@@ -1,1 +1,13 @@
-
+__add__
+   def __init__(self,thuc,ao):
+      self.thuc= thuc
+      self.ao= ao 
+   def __add__(self,other):
+      self.thuc+= other.thuc
+      self.ao += other.ao 
+      return soPhuc(self.thuc,self.ao)
+   def __str__(self):
+      return f'{self.thuc} + {self.ao}j'
+a= soPhuc(1,2)
+b= soPhuc(3,3)
+print(a+b)
