59d8ae0c6c9e7dd3fe0ba808b35fe6c3aecdd596
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-30 21:03:15 +0700

    Update Nạp chồng toán tử.py

--- Nạp chồng toán tử.py
+++ Nạp chồng toán tử.py
@@ -1,13 +1,20 @@
 __add__
+
+## dùng tmp để khi nạp chồng thì a không bị thay đổi
+class soPhuc:
    def __init__(self,thuc,ao):
       self.thuc= thuc
       self.ao= ao 
    def __add__(self,other):
-      self.thuc+= other.thuc
-      self.ao += other.ao 
-      return soPhuc(self.thuc,self.ao)
+      tmp1 = self.thuc+ other.thuc
+      tmp2 = self.ao +other.ao
+      return soPhuc(tmp1,tmp2)
    def __str__(self):
       return f'{self.thuc} + {self.ao}j'
 a= soPhuc(1,2)
 b= soPhuc(3,3)
-print(a+b)
+c= a+b
+print(c)
+print(a)
+
+
