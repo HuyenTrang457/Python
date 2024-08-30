628be803c2a7ff546716acec138b6140ca3c07bf
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-30 20:33:53 +0700

    Update Datetime.py

--- Datetime.py
+++ Datetime.py
@@ -1,3 +1,9 @@
+CCHUẨN HÓA NGÀY SINH VỀ DẠNG .....
+   def chuanHoa(self):
+      date_obj = datetime.strptime(self.ngaySinh, "%d/%m/%Y")
+    # Định dạng lại ngày tháng theo mong muốn
+      self.ngaySinh= date_obj.strftime("%d/%m/%Y")
+
 import datetime 
 x=datetime.datetime.now()
 print(x.strftime("%A")) -->Saturday
