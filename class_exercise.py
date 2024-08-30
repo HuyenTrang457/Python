4e3d66b19f5e7039305024794beadc9e264e41f7
HuyenTrang_ <155938605+HuyenTrang457@users.noreply.github.com>
2024-08-30 12:57:01 +0700

    Update class_exercise.py

--- class_exercise.py
+++ class_exercise.py
@@ -10,3 +10,24 @@ ten="nguyen thi huyen trang"
 diem=11
 a= sinhVien(ten,diem)
 print(a)
+
+-----------LUONG NHAN VIEN ----------------------------
+class nhanVien:
+   def __init__(self, maNv,hoTen,luongCoBan):
+      self.maNv= maNv
+      self.hoTen= hoTen
+      self.luongCoBan = luongCoBan
+   def luong(self):
+      s1 = self.maNv[0:2]  
+      s2 = int(self.maNv[2:4])
+      if s1=="HT":
+         phuCap= 2000
+      elif s1=="HP":
+         phuCap= 9000
+      elif s1=="GV":
+         phuCap=500
+      return phuCap*s2+self.luongCoBan
+   def __str__(self):
+      return f'ma nhan vien: {self.maNv}\nho ten: {self.hoTen}\nluong chinh: {self.luong()}'
+a= nhanVien("HP03","hoang trang",10000)
+print(a)
